ca1fbe15c1b09d300dd1ee021ce5a9b176d089ed
Faniriantsoa RANDRIAHARIMINO <frandriaharimino@yahoo.com>
2023-10-30 08:20:17 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Faniriantsoa RANDRIAHARIMINO</h1>
+    </>
+  )
+}
+
+export default App
