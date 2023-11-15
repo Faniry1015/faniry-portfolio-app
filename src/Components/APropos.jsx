b52e6d51705fd45c4a695687d4ca7730be3fbf45
Faniriantsoa RANDRIAHARIMINO <frandriaharimino@yahoo.com>
2023-11-15 18:33:50 +0300

    Apropos

--- src/Components/APropos.jsx
+++ src/Components/APropos.jsx
@@ -1,4 +1,4 @@
-import { Box, Stack } from '@mui/system'
+import { Box, bgcolor } from '@mui/system'
 import { Typography, Grid } from '@mui/material'
 import React from 'react'
 
@@ -8,7 +8,7 @@ function APropos() {
             <Typography variant='h3' color='secondary' id='propos'>
                 A propos de moi
             </Typography>
-            <Grid container spacing={0} sx={{alignItems: 'center'}}>
+            <Grid container spacing={0} sx={{ alignItems: 'center' }}>
                 <Grid xs={12} md={5} display={{ xs: 'none', md: 'block' }}>
                     <Box><img src="src\assets\images\webdevVector.png" alt="web developer" style={{ maxWidth: '100%' }} /></Box>
                 </Grid>
@@ -17,9 +17,8 @@ function APropos() {
                         <Typography>
                             Bonjour, je suis Faniriantsoa RANDRIAHARIMINO, passionné par les nouvelles technologies et fervent explorateur du monde du développement. Ma fascination pour l'innovation technologique a été le moteur de ma transition vers ce domaine.
                         </Typography>
-                        Récemment, j'ai participé à un programme de formation financé par l'Organisation Internationale de la Francophonie pour approfondir mes compétences en tant que développeur.
                         <Typography>
-                            Ingénieur Agronome de formation, mon parcours professionnel en tant que chef de service du système d'information et du suivi-évaluation au Ministère de l'Agriculture et de l'Élevage à Madagascar a renforcé ma passion pour la tech. Cette expérience a contribué à façonner ma compréhension des technologies de l'information et a constitué une base solide pour ma transition vers le développement.
+                            Ingénieur Agronome de formation, mon parcours professionnel en tant que premier responsable du système d'information au sein d'une entité ministérielle a renforcé ma passion pour la tech. Cette expérience a contribué à façonner ma compréhension des technologies de l'information et a constitué une base solide pour ma transition vers le développement.
                         </Typography>
                         <Typography>
                             Je suis ouvert à des collaborations passionnantes et enthousiaste à l'idée d'explorer de nouveaux horizons dans le monde du développement.
