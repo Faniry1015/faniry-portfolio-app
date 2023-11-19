bd6a975d028f5678cd1692dd36e54279fe000461
Faniriantsoa RANDRIAHARIMINO <frandriaharimino@yahoo.com>
2023-11-19 12:43:23 +0300

    begin to create projects

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -2,6 +2,26 @@ import React from 'react'
 import { Box, Paper, Typography, useTheme } from '@mui/material'
 
 function Portfolio() {
+    const projects = [
+        {
+            id: 1,
+            title: 'Superman React',
+            category: 'front',
+            link: 'https://sayna-react-superman-base.web.app/',
+            githubRepo: 'https://github.com/Faniry1015/SAYNA-REACT-SUPERMAN-112022',
+            img: '',
+            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis nesciunt doloribus facere dolor aspernatur a. Ad recusandae, odit adipisci voluptatibus obcaecati qui dolorem totam commodi quas, provident maxime maiores voluptatum?'
+        },
+        {
+            id: 2,
+            title: 'Batman React',
+            category: 'front',
+            link: 'https://batman-sayna-react-app.web.app/',
+            githubRepo: 'https://github.com/Faniry1015/SAYNA-REACTJS-BATMANREACT',
+            img: '',
+            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis nesciunt doloribus facere dolor aspernatur a. Ad recusandae, odit adipisci voluptatibus obcaecati qui dolorem totam commodi quas, provident maxime maiores voluptatum?'
+        },
+    ]
     const theme = useTheme()
   return (
     <Box component='section'  className='mainSection' id='portfolio'>
